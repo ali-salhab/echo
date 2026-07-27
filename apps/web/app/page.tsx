8772c4a664b901ce582431ed9b4dc7d349821c8d
ali-salhab <alysalhab5@gmail.com>
2026-07-27 09:17:43 +0200

    add package for  convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,10 @@ import { Button } from "@workspace/ui/components/button"
 import { multilple } from "@workspace/math/multiple"
 
 import { Input } from "@workspace/ui/components/input"
-import { use } from "react"
+import React, { use } from "react"
+import { useQuery } from "convex/react"
+
+import { api } from "@workspace/backend/api"
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col bg-amber-500 p-6">
--- apps/widget/app/page.js
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <div className="flex min-h-svh p-6"></div>;
-}
