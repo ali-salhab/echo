6826076d087821f7a93e88381b72d6377a35c125
ali-salhab <alysalhab5@gmail.com>
2026-08-01 06:50:58 +0200

    feat :add vapi to dashboard

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -3,13 +3,26 @@
 import React, { use } from "react"
 import { useQuery } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
+import { useVapi } from "@/modules/widget/hooks/use-vapi"
+import { Button } from "@workspace/ui/components/button"
 export default function Page() {
-  const users = useQuery(api.users.getMany)
-  console.log("users", users)
+  const {
+    vapi,
+    isConnecting,
+    isSpeaking,
+    transcript,
+    isConnected,
+    startCall,
+    endCall,
+  } = useVapi()
   return (
     <div className="flex min-h-screen flex-col bg-amber-500 p-6">
-      <h1>widget app</h1>
-      <h1>{users ? `Users: ${users[0]?.name}` : "Loading..."}</h1>
+      <Button onClick={startCall}>Start Call</Button>
+      <Button onClick={endCall}>End Call</Button>
+      <p>isConnecting: {isConnecting.toString()}</p>
+      <p>isSpeaking: {isSpeaking.toString()}</p>
+      <p>isConnected: {isConnected.toString()}</p>
+      <p>{JSON.stringify(transcript, null, 2)}</p>
     </div>
   )
 }
