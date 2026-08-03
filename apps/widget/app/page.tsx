ec630cf74122256875fc3f7d4c35028d77a5e199
ali-salhab <alysalhab5@gmail.com>
2026-08-03 21:08:10 +0200

    feat: enhance widget layout with header and footer components, and import global styles

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -2,26 +2,15 @@
 
 import React, { use } from "react"
 
-import { useVapi } from "@/modules/widget/hooks/use-vapi"
-import { Button } from "@workspace/ui/components/button"
-export default function Page() {
-  const {
-    vapi,
-    isConnecting,
-    isSpeaking,
-    transcript,
-    isConnected,
-    startCall,
-    endCall,
-  } = useVapi()
-  return (
-    <div className="flex min-h-screen flex-col bg-amber-500 p-6">
-      <Button onClick={startCall}>Start Call</Button>
-      <Button onClick={endCall}>End Call</Button>
-      <p>isConnecting: {isConnecting.toString()}</p>
-      <p>isSpeaking: {isSpeaking.toString()}</p>
-      <p>isConnected: {isConnected.toString()}</p>
-      <p>{JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  )
+import WidgetView from "@/modules/ui/views/widget-view"
+
+import WidgetFooter from "@/modules/ui/components/widget-footer"
+interface Props {
+  searchParams: Promise<{
+    organizationId: string
+  }>
+}
+export default function Page({ searchParams }: Props) {
+  const { organizationId } = use(searchParams)
+  return <WidgetView organizationId={organizationId} />
 }
--- apps/widget/modules/ui/views/widget-view.tsx
+++ apps/widget/modules/ui/views/widget-view.tsx
@@ -1,10 +1,23 @@
 import React from "react"
+import WidgetFooter from "../components/widget-footer"
+import WidgetHeader from "../components/widget-header"
 
 interface Props {
   organizationId: string
 }
-const WidgetView = ({ organizationId }: Props) => {
-  return <div>WidgetView: {organizationId}</div>
+const WidgetView = (p: Props) => {
+  return (
+    <div className="flex h-screen flex-col overflow-hidden rounded-b-2xl border">
+      <WidgetHeader>
+        <div className="flex flex-col justify-between px-2 py-1">
+          <p className="text-3xl font-semibold">hey there👌 </p>
+          <p className="font-ligth text-lg"> How Can We help you Today ?</p>
+        </div>
+      </WidgetHeader>
+      <div className="flex flex-1">{p.organizationId}</div>
+      <WidgetFooter />
+    </div>
+  )
 }
 
 export default WidgetView
